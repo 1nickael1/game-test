863ad318d4d58c0cf05177996a90bcbd2286389b
Nickael <nbruzzi@sonco.com>
2022-11-10 21:41:34 -0300

    fix xp

--- src/store/index.ts
+++ src/store/index.ts
@@ -81,7 +81,7 @@ export const useStore = create(
     
                     if (newEnemy.life <= 0) {
                         // @ts-ignore
-                        const XpReceived = getRandomNumberBetweenMaxAndMin( (originalEnemy.life * 0.5) + ((newHero.level * newEnemy.level) * 1.9), (newHero.xp.max* 0.1));
+                        const XpReceived = getRandomNumberBetweenMaxAndMin( (originalEnemy.life * 0.5) + (newHero.xp.max* 0.1), (newHero.xp.max* 0.1));
 
                         levelUp(XpReceived);
 
